c010f176b0fd5834f416963ece648abe47f01730
Chiel92 <ctje92@gmail.com>
2014-12-24 15:23:07 +0100

    operators working

--- cython/main.pyx
+++ cython/main.pyx
@@ -1,13 +1,16 @@
 from bitset cimport uint128, tostring, index, size, invert
 from fitnessfunctions import count_ones, lin_scaled_count_ones, td_trap, tn_trap
+from operators import two_point_crossover, uniform_crossover
 
 
 # x contains a number consisting of more than 64 1's
-cdef uint128 x = (<uint128>1 << 100)
+cdef uint128 x = (<uint128>1 << 100) - 1
+cdef uint128 y = (<uint128>0 << 50)
 
 print(tostring(x))
-print(count_ones(x))
-print(lin_scaled_count_ones(x))
-print(td_trap(x))
-print(tn_trap(x))
+print(tostring(y))
+
+fx, fy = two_point_crossover(x, y)
+print(tostring(fx))
+print(tostring(fy))
 
--- cython/operators.pyx
+++ cython/operators.pyx
@@ -2,29 +2,63 @@
 This module contains all kinds of operators.
 """
 
-from libc.stdlib cimport rand
+from bitset cimport uint128, tostring, invert
+from libc.stdlib cimport rand, srand
+from libc.time cimport time
+
+
+# Seed the random number generator
+srand(time(NULL));
 
 
 cdef extern from "stdlib.h":
     int RAND_MAX
 
 
-ctypedef unsigned long long ull
-
-
-cdef struct Pair:
-    ull x
-    ull y
-
-
 cdef int randomint(int upperbound):
     return <int> ((rand() / <float>RAND_MAX) * upperbound)
 
 
-cdef Pair two_point_crossover(ull x, ull y):
-    cdef int k = randomint(100)
-    cdef ull part1 = (1 << k) - 1
-    cdef ull part2 = ~part1
+cdef uint128 randuint128():
+    cdef uint128 result = rand()
+    for _ in range(3):
+        result <<= 32
+        result |= rand()
+    return result
 
-    return Pair((x & part1) | (y & part2), (y & part1) | (x & part2))
+
+def two_point_crossover(uint128 x, uint128 y):
+    cdef int k1 = randomint(100)
+    cdef int k2 = randomint(100)
+    k1, k2 = max(k1, k2), min(k1, k2)
+    #print(k1, k2)
+
+    cdef uint128 m = ((<uint128>1 << k1) - 1) #011
+    cdef uint128 part1 = invert(m, 100) #100
+    cdef uint128 part3 = ((<uint128>1 << k2) - 1) #001
+    cdef uint128 part2 = m - part3 #010
+    #print(tostring(m))
+    #print(tostring(part1))
+    #print(tostring(part2))
+    #print(tostring(part3))
+
+    return (x & part1) | (y & part2) | (x & part3), (y & part1) | (x & part2) | (y & part3)
+
+
+def uniform_crossover(uint128 x, uint128 y):
+    cdef uint128 k = randuint128()
+    cdef uint128 fx = 0
+    cdef uint128 fy = 0
+
+    cdef uint128 i = 1
+    while i:
+        if i & k:
+            fx |= x & i
+            fy |= y & i
+        else:
+            fx |= y & i
+            fy |= x & i
+        i <<= 1
+
+    return fx, fy
 
